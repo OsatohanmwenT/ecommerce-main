cb064fbe6a86f74ac7e61a9c294e77153328bb2b
OsatohanmwenT <143819001+OsatohanmwenT@users.noreply.github.com>
2024-07-14 10:55:40 +0100

    Update OverLay.jsx

--- src/components/OverLay.jsx
+++ src/components/OverLay.jsx
@@ -26,7 +26,7 @@ export default function Overlay({ images, currentIndex, handleThumbnailClick, ha
                             className={`mini ${index === currentIndex ? "active" : ""}`} // Apply 'active' class to the current thumbnail
                             onClick={() => handleThumbnailClick(index)} // Update main image on thumbnail click
                         >
-                            <img className="product" src={image.replace('.jpg', '-thumbnail.jpg')} alt={`image-product-${index + 1}`} />
+                            <img className="product" src={image.replace(".jpg", "-thumbnail.jpg")} alt={`image-product-${index + 1}`} />
                             <div className="cover"></div>
                         </picture>
                     ))}
