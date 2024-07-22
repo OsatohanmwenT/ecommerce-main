ff5b9edfafcd44c23d6e33944dc66375c5bdd128
OsatohanmwenT <143819001+OsatohanmwenT@users.noreply.github.com>
2024-07-22 15:53:42 +0100

    Update App.jsx

--- src/App.jsx
+++ src/App.jsx
@@ -10,10 +10,10 @@ import image1 from "/images/image-product-1.jpg";
 import image2 from "/images/image-product-2.jpg";
 import image3 from "/images/image-product-3.jpg";
 import image4 from "/images/image-product-4.jpg";
-import thumbnail1 from './images/image-product-1-thumbnail.jpg';
-import thumbnail2 from './images/image-product-2-thumbnail.jpg';
-import thumbnail3 from './images/image-product-3-thumbnail.jpg';
-import thumbnail4 from './images/image-product-4-thumbnail.jpg';
+import thumbnail1 from '/images/image-product-1-thumbnail.jpg';
+import thumbnail2 from '/images/image-product-2-thumbnail.jpg';
+import thumbnail3 from '/images/image-product-3-thumbnail.jpg';
+import thumbnail4 from '/images/image-product-4-thumbnail.jpg';
 
 export default function App() {
   const [itemCount, setItemCount] = useState(0);
